c36d6c7bb66fa8a302724faa376695f4badf2ac3
Kenneth Hunter <csuhermit@hotmail.com>
2016-03-23 09:14:03 -0400

    Kenneth Hunter CustomScan added comments

--- src/csu/csci325/CustomScan.java
+++ src/csu/csci325/CustomScan.java
@@ -4,27 +4,43 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 
 /**
- * Created by hunterd on 3/14/2016.
+ * Author: Kenneth Hunter
+ * Description: Class scans a user defined range of ports for open ports. Instantiate a CustomScan
+ * object, and the user can define a custom range, scan, and then define a different custom
+ * range and scan with the same object.
  */
+
 public class CustomScan extends Socket {
-    private String mIP = "127.0.0.1";
-    private int mStartPort = 0;
-    private int mEndPort = 65535;
-    private int mTimeout = 1000;
-    private boolean[] mPortsStatus;
+    private String mIP = "127.0.0.1";       // IP address of computer to scan. Default is local.
+    private int mStartPort = 0;             // Starting port of the range to scan. Default is 0.
+    private int mEndPort = 65535;           // Ending port of the range to scan. Default is 65535.
+    private int mTimeout = 1000;            // Timeout in ms for individual port query. Default is 1 sec.
+    private boolean[] mPortsStatus;         // Array of bools to indicate whether socket is open
+                                            // (true) or closed (false).
 
     public CustomScan() {}
 
+    /*
+    * Method scans for open ports in the range from mStartPort to mEndPort and stores the results
+    * in mPortsStatus.
+     * Returns: boolean to indicate whether port range is valid. True = valid range.
+    */
+
     public boolean getOpenPorts() {
+        // Declare variables
         Socket sock;
         InetSocketAddress isa;
 
+        // Check if port range is valid
         if(mEndPort-mStartPort < 0 ) {
             return false;
         }
 
+        // Declare a boolean array of size of the range of ports
         mPortsStatus = new boolean[mEndPort-mStartPort];
 
+        // Print scanning a continue to print dots as long as the scanning continues
+        // so the user knows the program hasn't locked.
         System.out.print("\r\nScanning");
 
         for (int i = 0; i < mPortsStatus.length; i++) {
@@ -32,26 +48,35 @@ public class CustomScan extends Socket {
                 System.out.print(".");
             }
 
+            // If we can connect to the socket, it's open. Mark that element in the array as
+            // true. If there's an exception, it's closed, so mark it false.
             try {
                 isa = new InetSocketAddress(mIP,i+mStartPort);
                 sock  = new Socket();
                 sock.connect(isa, mTimeout);
                 sock.close();
                 mPortsStatus[i] = true;
-//                System.out.println(i);
             } catch (Exception ex) {
                 mPortsStatus[i] = false;
             }
         }
 
+        // Everything went okay, so return true.
         return true;
     }
 
+    // Getters
     public int getTimeout () {return mTimeout;}
     public String getIP() {return mIP;}
     public int getStartPort() {return mStartPort;}
     public int getEndPort() {return mEndPort;}
 
+    // Setters
+    /*
+    * Method sets the starting port for the scan. If it's outside the valid range of
+    * ports (0 to 65535) or if it's not an int, return false. Otherwise return true.
+    * Returns: boolean to indicate if the port is valid. True = valid.
+     */
     public boolean setStartPort(int startPort) {
         if(0 <= startPort && startPort <= 65535) {
             try {
@@ -64,6 +89,11 @@ public class CustomScan extends Socket {
         else return false;
     }
 
+    /*
+   * Method sets the ending port for the scan. If it's outside the valid range of
+   * ports (0 to 65535) or if it's not an int, return false. Otherwise return true.
+   * Returns: boolean to indicate if the port is valid. True = valid.
+    */
     public boolean setEndPort(int endPort) {
         if(0 <= endPort && endPort <= 65535) {
             try {
@@ -76,6 +106,11 @@ public class CustomScan extends Socket {
         else return false;
     }
 
+    /*
+   * Method sets the timeout for the scan. If it's not an int, return false.
+   * Otherwise return true.
+   * Returns: boolean to indicate if the timeout was successfully set. True = success.
+    */
     public boolean setTimeout(int timeout) {
         try {
             mTimeout = timeout;
@@ -85,6 +120,11 @@ public class CustomScan extends Socket {
         }
     }
 
+    /*
+    * Method sets the IP address for the scan. It takes the IP address as a string
+    * in the form 127.0.0.1 and returns a bool to indicate if it was successfully set.
+    * Returns: boolean to indicate if the address was successfully set.
+     */
     public boolean setIP(String IP) {
         try {
             mIP = IP;
@@ -94,22 +134,38 @@ public class CustomScan extends Socket {
         }
     }
 
+    /*
+    * Method prints a list of the open ports to the screen.
+     */
     public void printPorts()
     {
-        //boolean portsStatus[] = this.getOpenPorts();
+        System.out.println('\n');
+        System.out.println("Open Ports for " + mIP + ": ");
 
-        System.out.println("\r\nOpen Ports for " + mIP + ": ");
-
-        for (int i = mStartPort; i < mEndPort - 1; i++) {
+        // Cycle through array and print element number for any that are true.
+        for (int i = mStartPort; i < mEndPort; i++) {
             if (mPortsStatus[i]) {
                 System.out.print(i + mStartPort + ", ");
             }
         }
 
-            if (mPortsStatus[mEndPort - 1]) {
-                System.out.print(mEndPort - 1 + ". ");
-            }
+        System.out.print('\b' + '\b');
+        System.out.println();
+    }
 
+    /*
+    * Sample main showing an example of how to use the CustomScan class.
+    */
+    public static void main (String[] args){
+
+        CustomScan cs = new CustomScan();
+        cs.setStartPort(0);
+        cs.setEndPort(50);
+        cs.setIP("127.0.0.1");
+        if(cs.getOpenPorts()) {
+            cs.printPorts();
+        }
+        else System.out.println("Invalid ports");
     }
 }
 
